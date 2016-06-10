de38583ba1bd9fb43a24aba5394f710ed35f8399
cmpu2015 <cmpu2015@CMPULB1D063874.ifam>
2016-06-10 15:36:18 -0300

    Criaçao das classes cliente e operario

---
+++ src/br/Cliente.java
@@ -0,0 +1,11 @@
+package br;
+
+public class Cliente extends Funcionario implements Autenticavel{
+	private String nome;
+	public boolean autenticar(int senha, String login){
+		return false;
+		
+		
+	}
+
+}
